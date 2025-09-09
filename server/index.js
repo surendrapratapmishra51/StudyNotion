2973e282d435ee39ff29e064a73c8f53851dd023
surendrapratapmishra51 <surendrapratapmishra51@gmail.com>
2025-09-10 01:42:51 +0530

    Update Cors

--- server/index.js
+++ server/index.js
@@ -23,7 +23,10 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
     cors({
-        origin:"http://localhost:3000",
+        origin:[
+            "http://localhost:3000", 
+            "https://study-notion-frontend-snowy-eta.vercel.app"
+        ],
         credentials:true,
     })
 )
